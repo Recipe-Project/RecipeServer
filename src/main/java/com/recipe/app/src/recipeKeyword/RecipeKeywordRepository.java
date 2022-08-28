792b045b214c60c705345af63128fee926f0a9e9
kimmjieun <kimmjieun@gmail.com>
2022-08-28 16:27:45 +0900

    인기 검색어 조회 API 수정 - 검색어 조회 기준 변경

--- src/main/java/com/recipe/app/src/recipeKeyword/RecipeKeywordRepository.java
+++ src/main/java/com/recipe/app/src/recipeKeyword/RecipeKeywordRepository.java
@@ -12,7 +12,8 @@ import java.util.List;
 public interface RecipeKeywordRepository extends CrudRepository<RecipeKeyword, Integer> {
     @Query(value="select keyword,count(keyword) as keywordCount\n" +
             "from RecipeKeyword\n" +
-            "where status = 'ACTIVE'\n" +
+            "where status = 'ACTIVE' " +
+            "       and createdAt BETWEEN concat(DATE_FORMAT(NOW()- INTERVAL 1 DAY,'%Y-%m-%d %H'),':00:00') AND concat(DATE_FORMAT(NOW(),'%Y-%m-%d %H'), ':00:00')\n" +
             "group by keyword\n" +
             "order by keywordCount desc limit 10;",nativeQuery = true)
     List<Object[]> findByBestKeywordTop10();
