132fceade3482c09799e7a2b1a94c4f24899f583
joona95 <waffle@vanpl.co.kr>
2023-06-24 11:58:10 +0900

    refactor: user 관련 DTO 정리 및 공통 내용 통일하여 작성

--- src/main/java/com/recipe/app/src/user/application/dto/UserDto.java
+++ src/main/java/com/recipe/app/src/user/application/dto/UserDto.java
@@ -3,6 +3,9 @@ package com.recipe.app.src.user.application.dto;
 import com.recipe.app.src.user.domain.User;
 import lombok.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class UserDto {
 
     @Getter
@@ -10,18 +13,54 @@ public class UserDto {
     @AllArgsConstructor
     @NoArgsConstructor
     public static class UserProfileRequest {
-
+        private String profilePhoto;
+        private String userName;
     }
 
     @Getter
     @AllArgsConstructor
     @NoArgsConstructor(access = AccessLevel.PRIVATE)
     public static class UserProfileResponse {
-        private Integer userIdx;
-        private String jwt;
+        private int userIdx;
+        private String socialId;
+        private String profilePhoto;
+        private String userName;
+        private long youtubeScrapCnt;
+        private long blogScrapCnt;
+        private long recipeScrapCnt;
+        private long myRecipeTotalSize;
+        private List<UserRecipeResponse> userRecipes;
 
-        public UserProfileResponse(User user, String jwt) {
-            this(user.getUserIdx(), jwt);
+        public UserProfileResponse(User user) {
+            this(user.getUserIdx(),
+                    user.getSocialId(),
+                    user.getProfilePhoto(),
+                    user.getUserName(),
+                    user.getScrapYoutubes().stream()
+                            .filter((scrap) -> scrap.getStatus().equals("ACTIVE"))
+                            .count(),
+                    user.getScrapBlogs().stream()
+                            .filter((scrap) -> scrap.getStatus().equals("ACTIVE"))
+                            .count(),
+                    user.getScrapPublics().stream()
+                            .filter((scrap) -> scrap.getStatus().equals("ACTIVE"))
+                            .count(),
+                    user.getUserRecipes().stream()
+                            .filter((scrap) -> scrap.getStatus().equals("ACTIVE"))
+                            .count(),
+                    user.getUserRecipes().stream()
+                            .filter((scrap) -> scrap.getStatus().equals("ACTIVE"))
+                            .map((r) -> new UserRecipeResponse(r.getUserRecipeIdx(), r.getThumbnail()))
+                            .collect(Collectors.toList())
+            );
         }
+    }
+
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor(access = AccessLevel.PRIVATE)
+    public static class UserRecipeResponse {
+        private int userRecipeIdx;
+        private String thumbnail;
     }
 }
