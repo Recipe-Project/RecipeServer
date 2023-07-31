f58a182c3aa4efd90e94aff718f2feb53949e019
joona95 <joona95@naver.com>
2023-07-31 23:14:07 +0900

    docs: 재료 목록 조회 응답값 설명 swagger 추가

--- src/main/java/com/recipe/app/src/ingredient/application/dto/IngredientDto.java
+++ src/main/java/com/recipe/app/src/ingredient/application/dto/IngredientDto.java
@@ -2,6 +2,7 @@ package com.recipe.app.src.ingredient.application.dto;
 
 import com.recipe.app.src.ingredient.domain.Ingredient;
 import com.recipe.app.src.ingredient.domain.IngredientCategory;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,15 +14,19 @@ import java.util.stream.Collectors;
 
 public class IngredientDto {
 
+
     @Getter
     @NoArgsConstructor(access = AccessLevel.PRIVATE)
+    @Schema(description = "재료 목록 응답 DTO")
     public static class IngredientsResponse {
+        @Schema(description = "냉장고 바구니 갯수")
         private int fridgeBasketCount;
-        private List<IngredientCategoryResponse> ingredients;
+        @Schema(description = "카테고리별 재료 목록")
+        private List<IngredientCategoryResponse> ingredientCategories;
 
         public IngredientsResponse(int fridgeBasketCount, Map<IngredientCategory, List<Ingredient>> ingredientsGroupingByIngredientCategory) {
             this.fridgeBasketCount = fridgeBasketCount;
-            this.ingredients = ingredientsGroupingByIngredientCategory.keySet().stream()
+            this.ingredientCategories = ingredientsGroupingByIngredientCategory.keySet().stream()
                     .map((category) -> new IngredientCategoryResponse(category, ingredientsGroupingByIngredientCategory.get(category)))
                     .collect(Collectors.toList());
         }
@@ -30,8 +35,11 @@ public class IngredientDto {
     @Getter
     @AllArgsConstructor
     public static class IngredientCategoryResponse {
+        @Schema(description = "재료 카테고리 고유 번호")
         private Long ingredientCategoryId;
+        @Schema(description = "재료 카테고리명")
         private String ingredientCategoryName;
+        @Schema(description = "재료 목록")
         private List<IngredientResponse> ingredients;
 
         public IngredientCategoryResponse(IngredientCategory category, List<Ingredient> ingredients) {
@@ -47,9 +55,12 @@ public class IngredientDto {
     @AllArgsConstructor
     @NoArgsConstructor(access = AccessLevel.PRIVATE)
     public static class IngredientResponse {
+        @Schema(description = "재료 고유 번호")
         private Long ingredientId;
+        @Schema(description = "재료명")
         private String ingredientName;
-        private String ingredientIcon;
+        @Schema(description = "재료 아이콘 url")
+        private String ingredientIconUrl;
 
         public IngredientResponse(Ingredient ingredient) {
             this(ingredient.getIngredientId(), ingredient.getIngredientName(), ingredient.getIngredientIconUrl());
