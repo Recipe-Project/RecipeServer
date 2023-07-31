82e244bfd147887c8d0bc3be54e3d87ad27fbb41
joona95 <joona95@naver.com>
2023-07-31 23:18:47 +0900

    feat: swagger 요청값, 응답값 content-type 설정 추가

--- src/main/java/com/recipe/app/config/SwaggerConfig.java
+++ src/main/java/com/recipe/app/config/SwaggerConfig.java
@@ -10,6 +10,9 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Configuration
 public class SwaggerConfig {
 
@@ -17,11 +20,26 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .useDefaultResponseMessages(false)
+                .consumes(getConsumeContentTypes())
+                .produces(getProduceContentTypes())
                 .select()
                 .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo());
+    }
+
+    private Set<String> getConsumeContentTypes() {
+        Set<String> consumes = new HashSet<>();
+        consumes.add("application/json;charset=UTF-8");
+        consumes.add("application/x-www-form-urlencoded");
+        return consumes;
+    }
+
+    private Set<String> getProduceContentTypes() {
+        Set<String> produces = new HashSet<>();
+        produces.add("application/json;charset=UTF-8");
+        return produces;
     }
 
     private ApiInfo apiInfo() {
