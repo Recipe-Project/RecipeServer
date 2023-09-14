a59b820928ce2b84c7abdeac8cb895b95968bcbc
joona95 <joona95@naver.com>
2023-09-14 23:17:53 +0900

    docs: 블로그 레시피 관련 swagger 정보 추가

--- src/main/java/com/recipe/app/src/recipe/api/BlogRecipeController.java
+++ src/main/java/com/recipe/app/src/recipe/api/BlogRecipeController.java
@@ -4,19 +4,24 @@ import com.recipe.app.common.response.BaseResponse;
 import com.recipe.app.src.recipe.application.BlogRecipeService;
 import com.recipe.app.src.recipe.application.SearchKeywordService;
 import com.recipe.app.src.recipe.application.dto.RecipeDto;
+import com.recipe.app.src.recipe.domain.BlogRecipe;
 import com.recipe.app.src.user.domain.SecurityUser;
 import com.recipe.app.src.user.domain.User;
 import com.recipe.app.src.user.exception.UserTokenNotExistException;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
 
-import java.util.List;
 import java.util.stream.Collectors;
 
 import static com.recipe.app.common.response.BaseResponse.success;
 
+@Api(tags = {"블로그 레시피 Controller"})
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/recipes/blog")
@@ -25,8 +30,17 @@ public class BlogRecipeController {
     private final BlogRecipeService blogRecipeService;
     private final SearchKeywordService recipeKeywordService;
 
+    @ApiOperation(value = "블로그 레시피 목록 조회 API")
     @GetMapping("")
     public BaseResponse<RecipeDto.RecipesResponse> getBlogRecipes(@ApiIgnore final Authentication authentication,
+                                                                  @ApiParam(name = "keyword", type = "String", example = "감자", value = "검색어")
+                                                                  @RequestParam(value = "keyword") String keyword,
+                                                                  @ApiParam(name = "page", type = "int", example = "0", value = "페이지")
+                                                                  @RequestParam(value = "page") int page,
+                                                                  @ApiParam(name = "size", type = "int", example = "20", value = "사이즈")
+                                                                  @RequestParam(value = "size") int size,
+                                                                  @ApiParam(name = "sort", type = "String", example = "조회수순(blogViews) / 좋아요순(blogScraps) / 최신순(blogRecipeId) = 기본값", value = "정렬")
+                                                                  @RequestParam(value = "sort") String sort) {
 
         if (authentication == null)
             throw new UserTokenNotExistException();
@@ -45,8 +59,9 @@ public class BlogRecipeController {
         return success(data);
     }
 
+    @ApiOperation(value = "블로그 레시피 상세 조회 API", notes = "블로그 레시피 상세 조회 시 조회수 올리기 위한 API")
     @GetMapping("/{blogRecipeId}")
-    public BaseResponse<Void> getBlogRecipe(final Authentication authentication, @PathVariable Long blogRecipeId) {
+    public BaseResponse<Void> getBlogRecipe(@ApiIgnore final Authentication authentication, @PathVariable Long blogRecipeId) {
 
         if (authentication == null)
             throw new UserTokenNotExistException();
@@ -57,8 +72,13 @@ public class BlogRecipeController {
         return success();
     }
 
+    @ApiOperation(value = "블로그 레시피 스크랩 목록 조회 API")
     @GetMapping("/scrap")
     public BaseResponse<RecipeDto.RecipesResponse> getScrapBlogRecipes(@ApiIgnore final Authentication authentication,
+                                                                       @ApiParam(name = "page", type = "int", example = "0", value = "페이지")
+                                                                       @RequestParam(value = "page") int page,
+                                                                       @ApiParam(name = "size", type = "int", example = "20", value = "사이즈")
+                                                                       @RequestParam(value = "size") int size) {
 
         if (authentication == null)
             throw new UserTokenNotExistException();
@@ -72,8 +92,9 @@ public class BlogRecipeController {
         return success(data);
     }
 
+    @ApiOperation(value = "블로그 레시피 스크랩 생성 API")
     @PostMapping("/{blogRecipeId}/scrap")
-    public BaseResponse<Void> postBlogRecipeScrap(final Authentication authentication, @PathVariable Long blogRecipeId) {
+    public BaseResponse<Void> postBlogRecipeScrap(@ApiIgnore final Authentication authentication, @PathVariable Long blogRecipeId) {
 
         if (authentication == null)
             throw new UserTokenNotExistException();
@@ -84,8 +105,9 @@ public class BlogRecipeController {
         return success();
     }
 
+    @ApiOperation(value = "블로그 레시피 스크랩 삭제 API")
     @DeleteMapping("/{blogRecipeId}/scrap")
-    public BaseResponse<Void> deleteBlogRecipeScrap(final Authentication authentication, @PathVariable Long blogRecipeId) {
+    public BaseResponse<Void> deleteBlogRecipeScrap(@ApiIgnore final Authentication authentication, @PathVariable Long blogRecipeId) {
 
         if (authentication == null)
             throw new UserTokenNotExistException();
