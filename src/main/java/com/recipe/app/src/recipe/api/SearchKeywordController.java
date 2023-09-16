a06d4708bd6d76914d17c2173b395bac0f73c4fb
joona95 <joona95@naver.com>
2023-09-16 17:48:47 +0900

    docs: 레시피 검색어 관련 swagger 정보 추가

--- src/main/java/com/recipe/app/src/recipe/api/SearchKeywordController.java
+++ src/main/java/com/recipe/app/src/recipe/api/SearchKeywordController.java
@@ -2,6 +2,8 @@ package com.recipe.app.src.recipe.api;
 
 import com.recipe.app.common.response.BaseResponse;
 import com.recipe.app.src.recipe.application.SearchKeywordService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,7 @@ import java.util.List;
 
 import static com.recipe.app.common.response.BaseResponse.success;
 
+@Api(tags = {"검색어 Controller"})
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/recipes")
@@ -18,10 +21,11 @@ public class SearchKeywordController {
 
     private final SearchKeywordService recipeKeywordService;
 
+    @ApiOperation(value = "인기 검색어 Top10 목록 조회 API")
     @GetMapping("/best-keywords")
     public BaseResponse<List<String>> getRecipeSearchTop10BestKeywords() {
 
-        List<String> data = recipeKeywordService.retrieveRecipesBestKeyword();
+        List<String> data = recipeKeywordService.retrieveRecipesDayBestKeyword();
 
         return success(data);
     }
