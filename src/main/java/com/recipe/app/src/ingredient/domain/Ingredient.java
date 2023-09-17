25ac0b7e25b36a11adf3cff460480fc859c6591d
joona95 <joona95@naver.com>
2023-09-17 15:18:00 +0900

    feat: 재료와 동일하다고 판단되는 재료명 목록 조회 추가

--- src/main/java/com/recipe/app/src/ingredient/domain/Ingredient.java
+++ src/main/java/com/recipe/app/src/ingredient/domain/Ingredient.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Getter
@@ -98,5 +100,69 @@ public class Ingredient {
     @Override
     public int hashCode() {
         return Objects.hash(ingredientId, ingredientName.replace(" ", ""));
+    }
+
+    public List<String> getSimilarIngredientName() {
+        if(ingredientName.equals("새우"))
+            return List.of("대하");
+        if(ingredientName.equals("대하"))
+            return List.of("새우");
+        if (ingredientName.equals("계란"))
+            return List.of("달걀");
+        if (ingredientName.equals("달걀"))
+            return List.of("계란");
+        if (ingredientName.equals("소고기"))
+            return List.of("쇠고기");
+        if (ingredientName.equals("쇠고기"))
+            return List.of("소고기");
+        if (ingredientName.equals("후추"))
+            return List.of("후춧가루");
+        if (ingredientName.equals("후춧가루"))
+            return List.of("후추");
+        if (ingredientName.equals("간마늘"))
+            return List.of("다진마늘");
+        if (ingredientName.equals("다진마늘"))
+            return List.of("간마늘");
+        if (ingredientName.equals("새싹채소"))
+            return List.of("어린잎채소", "무순");
+        if (ingredientName.equals("어린잎채소"))
+            return List.of("새싹채소");
+        if (ingredientName.equals("무순"))
+            return List.of("새싹채소");
+        if (ingredientName.equals("조개"))
+            return List.of("조갯살", "바지락");
+        if (ingredientName.equals("조갯살"))
+            return List.of("조개");
+        if (ingredientName.equals("바지락"))
+            return List.of("조개");
+        if (ingredientName.equals("케찹"))
+            return List.of("케첩");
+        if (ingredientName.equals("케첩"))
+            return List.of("케찹");
+        if (ingredientName.equals("소면"))
+            return List.of("국수");
+        if (ingredientName.equals("국수"))
+            return List.of("소면");
+        if (ingredientName.equals("김치"))
+            return List.of("김칫잎");
+        if (ingredientName.equals("김칫잎"))
+            return List.of("김치");
+        if(ingredientName.equals("고춧가루"))
+            return List.of("고추가루");
+        if (ingredientName.equals("고추가루"))
+            return List.of("고춧가루");
+        if (ingredientName.equals("올리브유"))
+            return List.of("올리브오일");
+        if (ingredientName.equals("올리브오일"))
+            return List.of("올리브유");
+        if (ingredientName.equals("파스타"))
+            return List.of("스파게티");
+        if (ingredientName.equals("스파게티"))
+            return List.of("파스타");
+        if (ingredientName.equals("포도씨유"))
+            return List.of("식용유");
+        if (ingredientName.equals("식용유"))
+            return List.of("포도씨유");
+        return new ArrayList<>();
     }
 }
