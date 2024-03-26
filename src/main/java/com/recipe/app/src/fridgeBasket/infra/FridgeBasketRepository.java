e207dd5029d4e602131b5c805c08232d3e5f505d
Joona Choi <juna052@naver.com>
2024-03-26 10:42:33 +0900

    refactor: 냉장고 바구니 레포지토리 통합

--- src/main/java/com/recipe/app/src/fridgeBasket/infra/FridgeBasketRepository.java
+++ src/main/java/com/recipe/app/src/fridgeBasket/infra/FridgeBasketRepository.java
@@ -1,14 +1,12 @@
 package com.recipe.app.src.fridgeBasket.infra;
 
 import com.recipe.app.src.fridgeBasket.domain.FridgeBasket;
-import com.recipe.app.src.ingredient.infra.IngredientEntity;
-import com.recipe.app.src.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface FridgeBasketJpaRepository extends JpaRepository<FridgeBasket, Long> {
+public interface FridgeBasketRepository extends JpaRepository<FridgeBasket, Long> {
 
     List<FridgeBasket> findByUserId(Long userId);
 
