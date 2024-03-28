bfa40e53a07d5b7eecdc75e2a8366a845ad6fe1f
Joona Choi <juna052@naver.com>
2024-03-28 18:12:08 +0900

    refactor: 유저 파사드 서비스 내 블로그, 유튜브 서비스 의존성 추가

--- src/main/java/com/recipe/app/src/user/application/UserFacadeService.java
+++ src/main/java/com/recipe/app/src/user/application/UserFacadeService.java
@@ -3,9 +3,11 @@ package com.recipe.app.src.user.application;
 import com.recipe.app.src.fridge.application.FridgeService;
 import com.recipe.app.src.fridgeBasket.application.FridgeBasketService;
 import com.recipe.app.src.ingredient.application.IngredientService;
-import com.recipe.app.src.recipe.application.BlogRecipeService;
 import com.recipe.app.src.recipe.application.RecipeService;
-import com.recipe.app.src.recipe.application.YoutubeRecipeService;
+import com.recipe.app.src.recipe.application.blog.BlogScrapService;
+import com.recipe.app.src.recipe.application.blog.BlogViewService;
+import com.recipe.app.src.recipe.application.youtube.YoutubeScrapService;
+import com.recipe.app.src.recipe.application.youtube.YoutubeViewService;
 import com.recipe.app.src.recipe.domain.Recipe;
 import com.recipe.app.src.user.application.dto.UserProfileResponse;
 import com.recipe.app.src.user.domain.User;
@@ -20,19 +22,23 @@ public class UserFacadeService {
 
     private final UserService userService;
     private final RecipeService recipeService;
-    private final YoutubeRecipeService youtubeRecipeService;
-    private final BlogRecipeService blogRecipeService;
+    private final YoutubeScrapService youtubeScrapService;
+    private final YoutubeViewService youtubeViewService;
+    private final BlogScrapService blogScrapService;
+    private final BlogViewService blogViewService;
     private final FridgeService fridgeService;
     private final FridgeBasketService fridgeBasketService;
     private final IngredientService ingredientService;
 
-    public UserFacadeService(UserService userService, RecipeService recipeService, YoutubeRecipeService youtubeRecipeService, BlogRecipeService blogRecipeService,
-                             FridgeService fridgeService, FridgeBasketService fridgeBasketService, IngredientService ingredientService) {
+    public UserFacadeService(UserService userService, RecipeService recipeService, YoutubeScrapService youtubeScrapService, YoutubeViewService youtubeViewService,
+                             BlogScrapService blogScrapService, BlogViewService blogViewService, FridgeService fridgeService, FridgeBasketService fridgeBasketService, IngredientService ingredientService) {
 
         this.userService = userService;
         this.recipeService = recipeService;
-        this.youtubeRecipeService = youtubeRecipeService;
-        this.blogRecipeService = blogRecipeService;
+        this.youtubeScrapService = youtubeScrapService;
+        this.youtubeViewService = youtubeViewService;
+        this.blogScrapService = blogScrapService;
+        this.blogViewService = blogViewService;
         this.fridgeService = fridgeService;
         this.fridgeBasketService = fridgeBasketService;
         this.ingredientService = ingredientService;
@@ -41,8 +47,8 @@ public class UserFacadeService {
     @Transactional(readOnly = true)
     public UserProfileResponse findUserProfile(User user) {
 
-        long youtubeScrapCnt = youtubeRecipeService.countYoutubeScrapByUser(user);
-        long blogScrapCnt = blogRecipeService.countBlogScrapByUser(user);
+        long youtubeScrapCnt = youtubeScrapService.countYoutubeScrapByUser(user);
+        long blogScrapCnt = blogScrapService.countBlogScrapByUser(user);
         long recipeScrapCnt = recipeService.countRecipeScrapByUser(user);
         List<Recipe> userRecipes = recipeService.getRecipesByUser(user, 0, 6).toList();
 
@@ -56,10 +62,10 @@ public class UserFacadeService {
         fridgeBasketService.deleteFridgeBasketsByUser(user);
         recipeService.deleteRecipesByUser(user);
         ingredientService.deleteIngredientsByUser(user);
-        youtubeRecipeService.deleteYoutubeScrapsByUser(user);
-        youtubeRecipeService.deteteYoutubeViewsByUser(user);
-        blogRecipeService.deleteBlogRecipeScrapsByUser(user);
-        blogRecipeService.deleteBlogRecipeViewByUser(user);
+        youtubeScrapService.deleteYoutubeScrapsByUser(user);
+        youtubeViewService.deleteYoutubeViewsByUser(user);
+        blogScrapService.deleteBlogRecipeScrapsByUser(user);
+        blogViewService.deleteBlogRecipeViewByUser(user);
 
         userService.deleteUser(user, request);
     }
