eb49ec6cd133c5e01b1b32be758596ea4cddeed1
Joona Choi <juna052@naver.com>
2024-03-28 11:19:14 +0900

    refactor: created date 자동 설정 위한 entity listener 어노테이션 추가

--- src/main/java/com/recipe/app/src/user/domain/JwtBlacklist.java
+++ src/main/java/com/recipe/app/src/user/domain/JwtBlacklist.java
@@ -5,16 +5,19 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.util.StringUtils;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 @Table(name = "JwtBlacklist")
 public class JwtBlacklist {
