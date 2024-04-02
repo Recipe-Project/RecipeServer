3fdb1b7ee214700747cf3457be256a553e648206
Joona Choi <juna052@naver.com>
2024-04-02 10:57:56 +0900

    chore: spring boot test profile 설정 추가

--- src/test/groovy/com/recipe/app/AppApplicationTests.java
+++ src/test/groovy/com/recipe/app/AppApplicationTests.java
@@ -2,7 +2,9 @@ package com.recipe.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest(properties = "classpath:application-test.yml")
 class AppApplicationTests {
 
