2c8a35a6a23817afe79fc9067e88444a878c9238
Joona Choi <juna052@naver.com>
2024-04-02 10:34:07 +0900

    chore: spring boot test 의 test yml 파일 설정 추가

--- src/test/groovy/com/recipe/app/AppApplicationTests.java
+++ src/test/groovy/com/recipe/app/AppApplicationTests.java
@@ -2,7 +2,9 @@ package com.recipe.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
+@TestPropertySource(locations = "classpath:application-test.yml")
 @SpringBootTest
 class AppApplicationTests {
 
