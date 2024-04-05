c026134cf4dff00917ffc0d62994491fbc1b66e3
Joona Choi <juna052@naver.com>
2024-04-05 17:12:23 +0900

    chore: redis 실행 위한 docker 관련 github actions 설정 추가

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,6 +9,32 @@ DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
 TIME_NOW=$(date +%c)
 
+# Installing docker engine if not exists
+if ! type docker > /dev/null #docker를 깔아주는 코드, EC2 인스턴스에는 아무것도 없기 때문에 직접 깔아줘야 한다.
+then
+  echo "docker does not exist"
+  echo "Start installing docker"
+  sudo apt-get update
+  sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+  sudo apt update
+  apt-cache policy docker-ce
+  sudo apt install -y docker-ce
+fi
+
+# Installing docker-compose if not exists
+if ! type docker-compose > /dev/null #docker-compose를 깔아주는 코드
+then
+  echo "docker-compose does not exist"
+  echo "Start installing docker-compose"
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.27.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+fi
+
+echo "start docker-compose up"
+sudo docker-compose -f $PROJECT_ROOT/deploy/docker-compose.yml up --build -d
+
 # build 파일 복사
 echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
 cp $PROJECT_ROOT/deploy/*.jar $JAR_FILE
