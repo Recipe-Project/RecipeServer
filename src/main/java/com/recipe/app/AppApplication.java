a6bebc2e78abf71297a3bada1bb310d98757a28f
Joona Choi <juna052@naver.com>
2024-04-06 15:09:05 +0900

    chore: jar 파일이 아니라 war 파일 빌드 및 실행되도록 수정

--- src/main/java/com/recipe/app/AppApplication.java
+++ src/main/java/com/recipe/app/AppApplication.java
@@ -2,16 +2,23 @@ package com.recipe.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @EnableJpaAuditing
 @SpringBootApplication
-public class AppApplication {
+public class AppApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+
+        return super.configure(builder);
+    }
 }
