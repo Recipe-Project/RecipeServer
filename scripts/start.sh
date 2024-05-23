2c26dd58d2ca6dca4c8a2b9f64a6befb089a44ee
Joona Choi <juna052@naver.com>
2024-05-23 21:44:29 +0900

    chore: jar 파일이 아니라 war 파일 빌드 및 실행되도록 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 PROJECT_ROOT="/home/ec2-user/app"
-JAR_FILE="$PROJECT_ROOT/app-0.0.1-SNAPSHOT.jar"
+WAR_FILE="$PROJECT_ROOT/app-0.0.1-SNAPSHOT.war"
 
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
@@ -10,16 +10,16 @@ DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 TIME_NOW=$(date +%c)
 
 # build 파일 복사
-echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
-cp $PROJECT_ROOT/deploy/*.jar $JAR_FILE
+echo "$TIME_NOW > $WAR_FILE 파일 복사" >> $DEPLOY_LOG
+cp $PROJECT_ROOT/deploy/*.war $WAR_FILE
 cp $PROJECT_ROOT/keystore.p12 /keystore.p12
 
 # 실행권한 추가
-chmod +x $JAR_FILE
+chmod +x $WAR_FILE
 
-# jar 파일 실행
-echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
+# war 파일 실행
+echo "$TIME_NOW > $WAR_FILE 파일 실행" >> $DEPLOY_LOG
+nohup java -jar $WAR_FILE > $APP_LOG 2> $ERROR_LOG &
 
-CURRENT_PID=$(pgrep -f $JAR_FILE)
+CURRENT_PID=$(pgrep -f $WAR_FILE)
 echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
