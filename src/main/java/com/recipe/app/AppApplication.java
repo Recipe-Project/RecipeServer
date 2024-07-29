060cd6052de8e4dfb8db4885920a8c0065ba7fed
Joona Choi <juna052@naver.com>
2024-07-29 22:35:26 +0900

    feat: feign client 사용 가능 어노테이션 추가

--- src/main/java/com/recipe/app/AppApplication.java
+++ src/main/java/com/recipe/app/AppApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @EnableJpaAuditing
+@EnableFeignClients
 @SpringBootApplication
 public class AppApplication extends SpringBootServletInitializer {
 
