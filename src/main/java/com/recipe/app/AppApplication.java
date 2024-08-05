e59ad68d416dcff8c6047b0e5f23167aca7c8fe0
Joona Choi <juna052@naver.com>
2024-08-05 10:48:46 +0900

    fix: 외부 API 결과값 비동기 저장에서 동기 저장으로 바꾸면서 async 어노테이션 제거

--- src/main/java/com/recipe/app/AppApplication.java
+++ src/main/java/com/recipe/app/AppApplication.java
@@ -6,13 +6,11 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @EnableJpaAuditing
 @EnableFeignClients
-@EnableAsync
 @SpringBootApplication
 public class AppApplication extends SpringBootServletInitializer {
 
