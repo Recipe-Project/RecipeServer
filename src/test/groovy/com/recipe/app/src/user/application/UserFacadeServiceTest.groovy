40fff62f5e7c83199e17dc7d7181018f84ef0527
Joona Choi <juna052@naver.com>
2024-08-08 20:42:58 +0900

    test: 메서드명 변경에 따른 테스트 수정

--- src/test/groovy/com/recipe/app/src/user/application/UserFacadeServiceTest.groovy
+++ src/test/groovy/com/recipe/app/src/user/application/UserFacadeServiceTest.groovy
@@ -105,7 +105,7 @@ class UserFacadeServiceTest extends Specification {
         1 * fridgeService.deleteFridgesByUserId(user.userId)
         1 * fridgeBasketService.deleteFridgeBasketsByUserId(user.userId)
         1 * recipeService.deleteAllByUserId(user.userId)
-        1 * ingredientService.deleteIngredientsByUserId(user.userId)
+        1 * ingredientService.deleteAllByUserId(user.userId)
         1 * youtubeScrapService.deleteAllByUserId(user.userId)
         1 * youtubeViewService.deleteAllByUserId(user.userId)
         1 * blogScrapService.deleteAllByUserId(user.userId)
