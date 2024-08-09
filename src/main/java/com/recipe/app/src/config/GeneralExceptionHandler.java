74e88bdbb7f780f9b634ed83b01b26c9776f9073
Joona Choi <juna052@naver.com>
2024-08-09 19:40:26 +0900

    fix: 예외 메세지 응답값으로 전달하도록 수정

--- src/main/java/com/recipe/app/src/config/GeneralExceptionHandler.java
+++ src/main/java/com/recipe/app/src/config/GeneralExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.recipe.app.src.config;
 
+import com.recipe.app.src.etc.exception.BadWordException;
 import com.recipe.app.src.etc.exception.NotFoundNoticeException;
 import com.recipe.app.src.fridge.exception.NotFoundFridgeException;
 import com.recipe.app.src.fridgeBasket.exception.NotFoundFridgeBasketException;
@@ -7,6 +8,7 @@ import com.recipe.app.src.ingredient.exception.NotFoundIngredientCategoryExcepti
 import com.recipe.app.src.ingredient.exception.NotFoundIngredientException;
 import com.recipe.app.src.recipe.exception.NotFoundRecipeException;
 import com.recipe.app.src.recipe.exception.NotFoundRecipeLevelException;
+import com.recipe.app.src.user.exception.ForbiddenAccessException;
 import com.recipe.app.src.user.exception.NotFoundUserException;
 import com.recipe.app.src.user.exception.UserTokenNotExistException;
 import org.slf4j.Logger;
@@ -29,9 +31,21 @@ public class GeneralExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e);
     }
 
-    @ExceptionHandler({UserTokenNotExistException.class})
+    @ExceptionHandler({UserTokenNotExistException.class, ForbiddenAccessException.class})
     public ResponseEntity<?> handleUnauthorizedException(Exception e) {
         log.error(e.getMessage());
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e);
+    }
+
+    @ExceptionHandler({BadWordException.class, IllegalArgumentException.class, NullPointerException.class})
+    public ResponseEntity<?> handleBadRequestException(Exception e) {
+        log.error(e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e);
+    }
+
+    @ExceptionHandler({Exception.class})
+    public ResponseEntity<?> handleException(Exception e) {
+        log.error(e.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e);
     }
 }
